110b677eb02f6877f60e1890c9e7f97a5bd16e8a
Baris <s.barissonmez@gmail.com>
2022-07-22 17:02:00 +0300

    Update ospf-neighbor-mismatch.py

--- ospf-neighbor-mismatch.py
+++ ospf-neighbor-mismatch.py
@@ -1,19 +1,3 @@
-"""
-Purpose: Automated test to check OSPF links share the same network/mask, area and timers.
-Be aware that as CDP is used you will see the mismatch from both traffic directions 
-ie: it will show a failure from R1 to R2, as well as R2 back to R1.
-This is the same mismatch shown from both perspectives of each device.
-
-Scope: This script uses CDP and was designed to operate between directly connected, OSPF-enabled devices.
-Switched shared segments will break the CDP logic. 
-
-
-Notes: This script uses Nornir2, not Nornir3. Install with:
-pip3 install nornir"<3"
-
-Tested: This script was tested using CSR1000v Images (Cisco IOS XE Software, Version 16.11.01b)
-"""
-
 import os
 import ipaddress
 import rich
